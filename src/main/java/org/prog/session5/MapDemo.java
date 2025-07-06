b18e8fcf04f5d69dbaf79d1e8b80416e9ac9e79b
SvitlanaFd <fotina.sdf@gmail.com>
2025-07-06 17:39:03 +0300

    Home work 5 (car owners)

--- src/main/java/org/prog/session5/MapDemo.java
+++ src/main/java/org/prog/session5/MapDemo.java
@@ -1,13 +1,61 @@
 package org.prog.session5;
 
+import org.prog.session1.Car;
+
 import java.util.*;
+
 
 //TODO: write Map with car owners as keys and owned cars as values
 //TODO: assign each car random color using randomColor() from this class
 
 public class MapDemo {
 
+    final static String User1 = "James Ostin";
+    final static String User2 = "Richard Line";
+    final static String User3 = "Kate Kotlin";
+
     public static void main(String[] args) {
+        Map<String, List<Car>> carOwners = new HashMap<>();
+
+        carOwners.put(User1, new ArrayList<>());
+
+        var car = new Car();
+        car.carName = "Rolls Royce";
+        car.color = randomColor();
+        carOwners.get(User1).add(car);
+
+        car = new Car();
+        car.carName = "BMW";
+        car.color = randomColor();
+        carOwners.get(User1).add(car);
+
+        carOwners.put(User2, new ArrayList<>());
+
+        car = new Car();
+        car.carName = "Mers";
+        car.color = randomColor();
+        carOwners.get(User2).add(car);
+
+        carOwners.put(User3, new ArrayList<>());
+
+        car = new Car();
+        car.carName = "Lexus";
+        car.color = randomColor();
+        carOwners.get(User3).add(car);
+
+        car = new Car();
+        car.carName = "Zaporozhets";
+        car.color = randomColor();
+        carOwners.get(User3).add(car);
+
+        for (var owner: carOwners.keySet()) {
+            System.out.println(owner);
+
+            for (var ownedCar: carOwners.get(owner)) {
+                System.out.println(ownedCar);
+            }
+        }
+
         // tax id, each tax id can have 1-5 FOPs
         Map<String, List<String>> map = new HashMap<>();
         map.put("17253761525", new ArrayList<>());
@@ -25,8 +73,7 @@ public class MapDemo {
     }
 
     public static String randomColor() {
-        Random random = new Random();
-        return switch (random.nextInt(5)) {
+        return switch ((int)(Math.random() * 5)) {
             case 0 -> "red";
             case 1 -> "green";
             case 2 -> "blue";
